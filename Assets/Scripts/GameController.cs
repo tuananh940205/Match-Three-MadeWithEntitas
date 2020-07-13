6f94afe072edf1eee4085e0ada95065f05983905
tuananh940205 <tuan.anh.94.02.05@gmail.com>
2020-07-13 18:14:05 +0700

    Initial commit

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,27 @@
+﻿using Entitas;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    private Systems _systems;
+    private Contexts _contexts;
+    void Start()
+    {
+        _contexts = Contexts.sharedInstance;
+
+        _systems = CreateSystems(_contexts);
+
+        _systems.Initialize();
+    }
+
+    void Update()
+    {
+        _systems.Execute();
+        _systems.Cleanup();
+    }
+
+    Systems CreateSystems(Contexts contexts)
+    {
+        return new Feature("Systems");
+    }
+}
